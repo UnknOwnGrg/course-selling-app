04f4b473d38378f064f05d74561d074c6cc8ba32
UnknOwnGrg <ishakgrg12@gmail.com>
2026-08-01 19:55:56 +0545

    Initial Setup

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import React from "react";
+
+const Logo = () => {
+  return <Image height={35} width={35} alt="Logo" src="./logo.svg" />;
+};
+
+export default Logo;
---
+++ app/(dashboard)/_components/Sidebar-route.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const SidebarRoutes = () => {
+  return <div>sidebar Routes</div>;
+};
+
+export default SidebarRoutes;
